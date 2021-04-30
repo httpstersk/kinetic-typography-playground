38f51ae444b7010626d9821c82d6fcfdc2683216
Lubos Belak <belak.lubos@gmail.com>
2021-04-30 12:19:41 +0200

    :sparkles: Controls for the `geometryDetail`

--- src/components/OOOFFFScene.tsx
+++ src/components/OOOFFFScene.tsx
@@ -1,6 +1,13 @@
-import { Box, Plane, Sphere, Text as DreiText, Torus } from '@react-three/drei';
+import {
+  Box,
+  Icosahedron,
+  Plane,
+  Sphere,
+  Text as DreiText,
+  Torus,
+} from '@react-three/drei';
 import { createPortal, MeshProps, useFrame } from '@react-three/fiber';
-import { buttonGroup, LevaInputs, useControls } from 'leva';
+import { buttonGroup, folder, LevaInputs, useControls } from 'leva';
 import React, { useMemo, useRef, useState } from 'react';
 import type { Mesh } from 'three';
 import { Color, PerspectiveCamera, Scene, WebGLRenderTarget } from 'three';
@@ -36,52 +43,63 @@ export const OOOFFFScene = (props: MeshProps) => {
   const [distortion, setDistortion] = useState(true);
   const [shadow, setShadow] = useState(true);
   const { camera, mesh, scene, texture } = useRenderTargetTexture();
-  const { fontSize, geometrySize, Repeats, Text } = useControls({
-    Geometry: buttonGroup({
-      Box: () => setActiveComponent('Box'),
-      Plane: () => setActiveComponent('Plane'),
-      Sphere: () => setActiveComponent('Sphere'),
-      Torus: () => setActiveComponent('Torus'),
-    }),
+  const { fontSize, geometryDetail, geometrySize, Repeats, Text } = useControls(
+    {
+      Geometry: {
+        value: activeComponent,
+        options: ['Box', 'Icosahedron', 'Plane', 'Sphere', 'Torus'],
+        onChange: setActiveComponent,
+      },
 
-    Text: {
-      type: LevaInputs.STRING,
-      value: 'OF',
-    },
+      Text: {
+        type: LevaInputs.STRING,
+        value: 'CREATE',
+      },
 
-    fontSize: {
-      label: 'Font Size',
-      min: 0.5,
-      max: 6,
-      step: 0.1,
-      value: 2,
-    },
+      fontSize: {
+        label: 'Font Size',
+        min: 0.1,
+        max: 6,
+        step: 0.1,
+        value: 1,
+      },
 
-    geometrySize: {
-      label: 'Geometry Size',
-      min: 1,
-      max: 10,
-      step: 0.1,
-      value: 3,
-    },
+      geometryDetail: {
+        label: 'Geometry Details',
+        min: 2,
+        max: 32,
+        step: 1,
+        value: 16,
+      },
 
-    Repeats: {
-      min: 1,
-      max: 6,
-      step: 1,
-      value: 6,
-    },
+      geometrySize: {
+        label: 'Geometry Size',
+        min: 1,
+        max: 10,
+        step: 0.1,
+        value: 3,
+      },
 
-    Distortion: {
-      value: !distortion,
-      onChange: () => setDistortion((state) => !state),
-    },
+      Repeats: {
+        min: 1,
+        max: 10,
+        step: 1,
+        value: 6,
+      },
 
-    Shadow: {
-      value: !shadow,
-      onChange: () => setShadow((state) => !state),
-    },
-  });
+      Distortion: {
+        value: !distortion,
+        onChange: () => setDistortion((state) => !state),
+      },
+
+      Shadow: {
+        value: !shadow,
+        onChange: () => setShadow((state) => !state),
+      },
+    }
+  );
+
+  const geometryRadius = geometrySize / 2;
 
   const material = (
     <KineticMaterial
@@ -104,18 +122,12 @@ export const OOOFFFScene = (props: MeshProps) => {
       />
 
       <SwitchGeometry active={activeComponent}>
-        <Plane
-          args={[geometrySize, geometrySize]}
-          attach="geometry"
-          name="Plane"
-          ref={mesh}
-        >
+        <Plane args={[geometrySize, geometrySize]} name="Plane" ref={mesh}>
           {material}
         </Plane>
 
         <Box
           args={[geometrySize, geometrySize, geometrySize]}
-          attach="geometry"
           name="Box"
           ref={mesh}
         >
@@ -123,22 +135,28 @@ export const OOOFFFScene = (props: MeshProps) => {
         </Box>
 
         <Sphere
-          args={[geometrySize / 2]}
-          attach="geometry"
+          args={[geometryRadius, geometryDetail, geometryDetail]}
           name="Sphere"
           ref={mesh}
         >
           {material}
         </Sphere>
 
+        <Icosahedron
+          args={[geometryRadius, geometryDetail]}
+          name="Icosahedron"
+          ref={mesh}
+        >
+          {material}
+        </Icosahedron>
+
         <Torus
           args={[
-            geometrySize / 2,
+            geometryRadius,
             geometrySize / 4,
             Math.pow(geometrySize, 2),
             Math.pow(geometrySize, 3),
           ]}
-          attach="geometry"
           name="Torus"
           ref={mesh}
         >
